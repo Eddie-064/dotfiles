1670f541b94b01ad957a840e23399e64fce52880
Eddie <jhih.ching@gmail.com>
2024-04-26 11:19:15 +0800

    Add lazyvim config

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ mv ~/.config/nvim{,.bak}
 mv ~/.local/share/nvim{,.bak}
 mv ~/.local/state/nvim{,.bak}
 mv ~/.cache/nvim{,.bak}
+rm -rf ~/.config/nvim/
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
